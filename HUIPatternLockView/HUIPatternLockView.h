b5d4675b765897062426effff2e85c2400b0745c
Tinghui <tinghui.zhang3@gmail.com>
2013-07-17 16:35:33 +0800

    Initial Commit

---
+++ HUIPatternLockView/HUIPatternLockView.h
@@ -0,0 +1,13 @@
+//
+//  HUIPatternLockView.h
+//  HUIPatternLockView
+//
+//  Created by ZhangTinghui on 13-7-17.
+//  Copyright (c) 2013年 ZhangTinghui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HUIPatternLockView : NSObject
+
+@end
---
+++ HUIPatternLockView/HUIPatternLockView.m
@@ -0,0 +1,13 @@
+//
+//  HUIPatternLockView.m
+//  HUIPatternLockView
+//
+//  Created by ZhangTinghui on 13-7-17.
+//  Copyright (c) 2013年 ZhangTinghui. All rights reserved.
+//
+
+#import "HUIPatternLockView.h"
+
+@implementation HUIPatternLockView
+
+@end
